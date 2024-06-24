824c0b6dd73ddb0dc0147a1d40c5d421730776f2
zheng peng <pengzheng@huishoubao.com.cn>
2024-06-24 18:38:45 +0800

    up

--- util.go
+++ util.go
@@ -4,6 +4,11 @@ import "reflect"
 
 var Time_format = "2024-01-02 15:04:05"
 
+//WrapFieldName 业务组件的字段名称全部改为内置，并约定调用该函数生成，使用者可以重写这个函数实现字段名定制化
+var WrapFieldName = func(fieldName string, business string) (wrappedFieldName string) {
+	return wrappedFieldName
+}
+
 func IsNil(v any) bool {
 	if v == nil {
 		return true
