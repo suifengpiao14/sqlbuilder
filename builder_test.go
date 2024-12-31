63c678b92db9a7179bfdbd55c0337636ebe68b26
zheng peng <pengzheng@huishoubao.com.cn>
2024-12-31 17:16:55 +0800

    a

--- builder_test.go
+++ builder_test.go
@@ -8,7 +8,16 @@ import (
 )
 
 func TestMysqlRealEscapeString(t *testing.T) {
-	str := `{   "originalMessage" : "{   \"app_key\" : \"34657042\",   \"biz_order_id\" : \"881723450001036535\",   \"order_status\" : \"5\",   \"order_sub_status\" : \"50\"}",   "type" : "4"}`
-	val := sqlbuilder.MysqlEscapeString(str)
-	fmt.Println(val)
+	t.Run("双引号", func(t *testing.T) {
+		str := `{   "originalMessage" : "{   \"app_key\" : \"34657042\",   \"biz_order_id\" : \"881723450001036535\",   \"order_status\" : \"5\",   \"order_sub_status\" : \"50\"}",   "type" : "4"}`
+		val := sqlbuilder.MysqlEscapeString(str)
+		fmt.Println(val)
+	})
+
+	t.Run("单引号", func(t *testing.T) {
+		val := `x-data='$tab({"eventName":"hello_world"})'`
+		val = sqlbuilder.MysqlEscapeString(val)
+		fmt.Println(val)
+	})
+
 }
