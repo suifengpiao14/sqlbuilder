dc139d07d1dc58371e77306b5564c509eabfd422
pz <2912150017@qq.com>
2025-07-26 20:00:28 +0800

    up

--- schema.go
+++ schema.go
@@ -27,8 +27,8 @@ type Schema struct {
 	Minimum   int        `json:"minimum"`   // 数字最小值
 	RegExp    string     `json:"regExp"`    //正则表达式
 
-	Primary       bool `json:"primary"` //是否为主键
-	Unique        bool `json:"unique"`  // 是否为唯一键
+	//Primary       bool `json:"primary"` //是否为主键
+	//Unique        bool `json:"unique"`  // 是否为唯一键
 	AutoIncrement bool `json:"autoIncrement"`
 	ShieldUpdate  bool `json:"shieldUpdate"` //这个地方还是保留，虽然可以结合场景和 sqlbuilder.ValueFnShieldForWrite 代替 但是比较麻烦，在set场景时，inert需要写值，update时不需要，所以需要这个字段
 	AllowZero     bool `json:"zeroAsEmpty"`  //0值是否当做空值处理，验证required 时有使用
